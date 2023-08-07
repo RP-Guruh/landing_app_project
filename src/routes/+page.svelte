c80fe511a34ed1b3bd46dc00589e8af50a1406ca
RP-Guruh <github.guruh@gmail.com>
2023-08-07 18:04:10 +0700

    add tailwind css

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -18,7 +18,8 @@
 			</picture>
 		</span>
 
-		to your new<br />SvelteKit app
+		<h1 class="text-red-700">to your new</h1>
+		<br />SvelteKit app
 	</h1>
 
 	<h2>
@@ -28,32 +29,6 @@
 	<Counter />
 </section>
 
-<style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 0.6;
-	}
+<style lang="postcss">
 
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	}
 </style>
